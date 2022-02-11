6d76b8a2c8e8cb73abfc6ed8c2bcb17b6abcd323
Linrui Wu <linrui@cs.ualberta.ca>
2022-02-10 19:52:07 -0700

    update assignment

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2022 Abram Hindle, Linrui Wu, https://github.com/tywtyw2002, and https://github.com/treedust
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+from urllib.request import urlcleanup
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -33,7 +34,12 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self,url):
+        parse_url = urllib.parse.urlparse(url)
+        host, port = parse_url.hostname, parse_url.port
+        if port == None: 
+            port = 80
+        return host, port
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,13 +47,13 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        return int(data.split('\r\n')[0].split()[1])
 
     def get_headers(self,data):
-        return None
+        return data.split('\r\n\r\n')[0]
 
     def get_body(self, data):
-        return None
+        return data.split('\r\n\r\n')[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,13 +74,53 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        #code = 500
+        #body = ''
+
+        host, port= self.get_host_port(url)
+        self.connect(host, int(port))
+        parse_url = urllib.parse.urlparse(url)
+        path = parse_url.path
+        if path == '':
+            path = '/'
+
+        request = 'GET {} HTTP/1.1\r\nHost: {}\r\nAccept: */*\r\nConnection: close\r\n\r\n'.format(path, host)   
+        self.sendall(request)
+
+        data = self.recvall(self.socket)
+        print(data)
+        self.close()
+        code = self.get_code(data)
+        body = self.get_body(data)
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        #code = 500
+        #body = ''
+
+        host, port= self.get_host_port(url)
+        self.connect(host, int(port))
+        parse_url = urllib.parse.urlparse(url)
+        path = parse_url.path
+        if path == '':
+            path = '/'
+
+        if args == None:
+            args = urllib.parse.urlencode('')
+        else:
+            args = urllib.parse.urlencode(args)
+
+        content_length = len(args)
+        content_type = 'application/x-www-form-urlencoded'
+        request = 'POST {} HTTP/1.1\r\nHost: {}s\r\nContent-Type: {}\r\nContent-Length: {}\r\nConnection: close\r\n\r\n{}'.format(path, host, content_type, content_length,args)
+        self.sendall(request)
+        
+        data = self.recvall(self.socket)
+        print(data)
+        self.close()
+        code = self.get_code(data)
+        body = self.get_body(data)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
